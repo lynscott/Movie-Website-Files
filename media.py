e17d6ba53d0878e0fbc9b143bbe98f3b0d9c12f5
Lyn Scott <lennord@gmail.com>
2017-07-27 10:20:42 -0700

    first commit

---
+++ media.py
@@ -0,0 +1,8 @@
+# Create Movie Class #
+
+class Movie():
+    def __init__(self, mov_title, mov_story, mov_poster, mov_trailer):
+        self.title = mov_title
+        self.storyline = mov_story
+        self.poster_image_url = mov_poster
+        self.trailer_youtube_url = mov_trailer
