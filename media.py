9951e6d11938bf11a3b8613570ea65ff0cacab52
Lyn Scott <lennord@gmail.com>
2017-07-28 13:17:03 -0700

    Review Note Changes

--- media.py
+++ media.py
@@ -1,6 +1,14 @@
 # Create Movie Class #
 
+
 class Movie():
+    """
+    Define your parameters for the movie class.
+    You can call the title, story, poster, and trailer vars
+    whatever you'd like or is easy for your to remember.
+    **Note** Don't change the name of what the variables will store to(left)
+    Only (right) and within the init f(x).
+    """
     def __init__(self, mov_title, mov_story, mov_poster, mov_trailer):
         self.title = mov_title
         self.storyline = mov_story
